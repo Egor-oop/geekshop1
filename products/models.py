c466f29a6dc6146a7a3853211cb0b4f5de50ca40
Egor <yegor.rabota.gu@mail.ru>
2021-08-20 16:31:02 +0300

    added __str__ methods for models.py

--- products/models.py
+++ products/models.py
@@ -5,6 +5,9 @@ class ProductsCategory(models.Model):
     name = models.CharField(max_length=64, unique=True)
     description = models.TextField(blank=True, null=True)
 
+    def __str__(self):
+        return self.name
+
 
 class Product(models.Model):
     name = models.CharField(max_length=256)
@@ -13,3 +16,6 @@ class Product(models.Model):
     price = models.DecimalField(max_digits=8, decimal_places=2, default=0)
     quantity = models.PositiveIntegerField(default=0)
     category = models.ForeignKey(ProductsCategory, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.name} | {self.category.name}'
