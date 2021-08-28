a29e86f764d964526ba7aa640e592308095cd703
Egor <yegor.rabota.gu@mail.ru>
2021-08-28 12:38:38 +0300

    lesson4 problem

--- users/views.py
+++ users/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponseRedirect
+from django.urls import reverse
+from django.contrib import auth
 
-# Create your views here.
+from users.froms import UserLoginForm
+
+
+def login(request):
+    if request.method == 'POST':
+        form = UserLoginForm(data=request.POST)
+        if form.is_valid():
+            username = request.POST['username']
+            password = request.POST['password']
+            user = auth.authenticate(username=username, password=password)
+            if user and user.is_active:
+                auth.login(request, user)
+                return HttpResponseRedirect(reverse('index'))
+    else:
+        form = UserLoginForm()
+
+    form = UserLoginForm()
+    context = {
+        'title': 'Geekshop - Авторизация',
+        'form': form
+    }
+    return render(request, 'users/login.html', context)
+
+def registration(request):
+    context = {
+        'title': 'Geekshop - Регистрация'
+    }
+    return render(request, 'users/register.html', context)
