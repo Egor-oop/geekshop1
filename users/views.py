3cee625b41038997c54cd7ae2e5738b36ce3c804
Egor <yegor.rabota.gu@mail.ru>
2021-09-04 10:42:21 +0300

    Added colors ofr toasts

--- users/views.py
+++ users/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, HttpResponseRedirect
 from django.urls import reverse
 from django.contrib import auth
+from django.contrib import messages
 
 from users.froms import UserLoginForm, UserRegistrationForm
 
@@ -30,6 +31,7 @@ def registration(request):
         form = UserRegistrationForm(data=request.POST)
         if form.is_valid():
             form.save()
+            messages.success(request, 'Вы успешно зарегестрировались')
             return HttpResponseRedirect(reverse('users:login'))
     else:
         form = UserRegistrationForm()
