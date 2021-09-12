93ece595dacdff8b813605425b301526703ce6f8
Egor <yegor.rabota.gu@mail.ru>
2021-09-12 19:01:39 +0300

    created controller for DELETE users in admin

--- admins/views.py
+++ admins/views.py
@@ -51,3 +51,10 @@ def admin_user_update(request, id):
         'form': form,
     }
     return render(request, 'admins/admin-users-update-delete.html', context)
+
+
+def admin_users_delete(request, id):
+    user = User.objects.get(id=id)
+    user.is_active = False
+    user.save()
+    return HttpResponseRedirect(reverse('admins:admin_users'))
