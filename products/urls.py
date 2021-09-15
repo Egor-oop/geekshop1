15412f09b524ab2efad2e701fbb77a2759178782
Egor <yegor.rabota.gu@mail.ru>
2021-09-15 10:58:19 +0300

    added paginator

--- products/urls.py
+++ products/urls.py
@@ -7,4 +7,5 @@ app_name = 'products'
 urlpatterns = [
     path('', products, name='product'),
     path('<int:category_id>/', products, name='category'),
+    path('page/<int:page>/', products, name='page'),
 ]
