4ddebaf31b1a727dce324e10daf23a3a3f1ff36b
Egor <yegor.rabota.gu@mail.ru>
2021-09-15 11:26:12 +0300

    added ListView and CreateView

--- admins/views.py
+++ admins/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, HttpResponseRedirect
-from django.urls import reverse
+from django.urls import reverse, reverse_lazy
 from django.contrib.auth.decorators import user_passes_test
+from django.views.generic.list import ListView
+from django.views.generic.edit import CreateView
 
 from users.models import User
 from admins.forms import UserAdminRegistrationForm, UserAdminProfileForm
@@ -14,30 +16,16 @@ def index(request):
     return render(request, 'admins/index.html', context)
 
 
-@user_passes_test(lambda u: u.is_staff)
-def admin_users(request):
-    context = {
-        'title': 'Geekshop - Пользователи',
-        'users': User.objects.all(),
-    }
-    return render(request, 'admins/admin-users.html', context)
+class UserListView(ListView):
+    model = User
+    template_name = 'admins/admin-users.html'
 
 
-@user_passes_test(lambda u: u.is_staff)
-def admin_users_create(request):
-    if request.method == 'POST':
-        form = UserAdminRegistrationForm(data=request.POST, files=request.FILES)
-        if form.is_valid():
-            form.save()
-            return HttpResponseRedirect(reverse('admins:admin_users'))
-    else:
-        form = UserAdminRegistrationForm()
-
-    context = {
-        'title': 'Geekshop - Создание пользователя',
-        'form': form,
-    }
-    return render(request, 'admins/admin-users-create.html', context)
+class UserCreateView(CreateView):
+    model = User
+    template_name = 'admins/admin-users-create.html'
+    form_class = UserAdminRegistrationForm
+    success_url = reverse_lazy('admins:admin_users')
 
 
 @user_passes_test(lambda u: u.is_staff)
