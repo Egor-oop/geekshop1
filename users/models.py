866565501a5d9535c5d247b9d549e7d476259ea8
Egor <yegor.rabota.gu@mail.ru>
2021-09-26 11:10:33 +0300

    added verify

--- users/models.py
+++ users/models.py
@@ -1,5 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+
+NOT_NULL = {'blank':True,'null':True}
+
 from django.utils.timezone import now
 from datetime import timedelta
 
@@ -8,13 +11,10 @@ class User(AbstractUser):
     image = models.ImageField(upload_to='users_images', blank=True, null=True)
 
     activation_key = models.CharField(max_length=128, blank=True)
-    activation_key_expires = models.DateTimeField(
-        default=now() + timedelta(hours=48)
-    )
+    activation_key_expires = models.DateTimeField(auto_now_add=True,blank=True,null=True)
 
 
     def is_activation_key_expired(self):
-        if now() <= self.activation_key_expires:
+        if now() <= self.activation_key_expires + timedelta(hours=48):
             return False
-        else:
-            return True
+        return True
