d4049e0591cda1d8ca89ddb20608d31f653a3708
Egor <yegor.rabota.gu@mail.ru>
2021-10-08 17:49:15 +0300

    Added models

--- ordersapp/models.py
+++ ordersapp/models.py
@@ -6,7 +6,7 @@ from products.models import Product
 
 class Order(models.Model):
     FORMING = 'FM'
-    SENT_TO_PROCEED ='STP'
+    SENT_TO_PROCEED = 'STP'
     PROCEEDED = 'PRD'
     PAID = 'PD'
     READY = 'RDY'
@@ -22,32 +22,32 @@ class Order(models.Model):
     )
     user = models.ForeignKey(settings.AUTH_USER_MODEL,
                              on_delete=models.CASCADE)
-    created = models.DateTimeField(verbose_name='создан', auto_now_add=True)
-    updated = models.DateTimeField(verbose_name='обновлен', auto_now=True)
+    created = models.DateTimeField(verbose_name='Создан', auto_now_add=True)
+    updated = models.DateTimeField(verbose_name='Обновлен', auto_now=True)
     status = models.CharField(verbose_name='статус',
                               max_length=3,
                               choices=ORDER_STATUS_CHOICES,
                               default=FORMING)
-    is_active = models.BooleanField(verbose_name='активен', default=True)
+    is_active = models.BooleanField(verbose_name='Активен', default=True)
 
     class Meta:
         ordering = ('-created',)
-        verbose_name = 'заказ'
-        verbose_name_plural = 'заказы'
+        verbose_name = 'Заказ'
+        verbose_name_plural = 'Заказы'
 
     def __str__(self):
-        return 'Текущий заказ: {}'.format(self.id)
+        return f'Текущий заказ: {self.id}'
 
     def get_total_quantity(self):
-        items = self.orderitems.select_related()
+        items = self.objects.select_related()
         return sum(list(map(lambda x: x.quantity, items)))
 
     def get_product_type_quantity(self):
-        items = self.orderitems.select_related()
+        items = self.objects.select_related()
         return len(items)
 
     def get_total_cost(self):
-        items = self.orderitems.select_related()
+        items = self.objects.select_related()
         return sum(list(map(lambda x: x.quantity * x.product.price, items)))
 
     # переопределяем метод, удаляющий объект
@@ -72,4 +72,3 @@ class OrderItem(models.Model):
 
     def get_product_cost(self):
         return self.product.price * self.quantity
-
